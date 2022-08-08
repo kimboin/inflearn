9a73b92679aea7f46d7f41719388eac4a5ccb728
kiko <suin@gimboin-ui-MacBook.local>
2022-08-08 16:06:42 +0900

    새로운 할인 정책 적용, 관심사의 분리

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,20 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPollicy;
 import hello.core.discount.FIxDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPollicy discountPollicy = new FIxDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPollicy discountPollicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPollicy discountPollicy) {
+        this.memberRepository = memberRepository;
+        this.discountPollicy = discountPollicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
